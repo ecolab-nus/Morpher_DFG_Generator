139121c5ade5b8e871be8bad494015a898ac6514
DhananjayaWijerathne <dmdwijerathne@gmail.com>
2021-06-23 17:11:16 +0800

    cleaned array add run pass

--- applications/array_add/run_pass.sh
+++ applications/array_add/run_pass.sh
@@ -4,7 +4,6 @@ rm *_munittrace.log
 rm memtraces/*
 clang -D CGRA_COMPILER -target i386-unknown-linux-gnu -c -emit-llvm -O2 -fno-tree-vectorize -fno-inline -fno-unroll-loops array_add.c -S -o array_add.ll
 opt -gvn -mem2reg -memdep -memcpyopt -lcssa -loop-simplify -licm -loop-deletion -indvars -simplifycfg -mergereturn -indvars array_add.ll -S -o array_add_opt.ll
-#opt -load ~/manycore/cgra_dfg/buildeclipse/skeleton/libSkeletonPass.so -fn $1 -ln $2 -ii $3 -skeleton integer_fft_gvn.ll -S -o integer_fft_gvn_instrument.ll
 
 opt -load ../../build/skeleton/libSkeletonPass.so -fn $1 -skeleton array_add_opt.ll -S -o array_add_opt_instrument.ll
 
