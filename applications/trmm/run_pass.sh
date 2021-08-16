77e3c251f59ab679877208a2c0deb2cc3f0ce600
WuDan0399 <wooden0399@gmail.com>
2021-08-16 10:23:29 +0800

    add trmm

---
+++ applications/trmm/run_pass.sh
@@ -0,0 +1,17 @@
+rm *_looptrace.log
+rm *_loopinfo.log
+rm *_munittrace.log
+rm memtraces/*
+
+clang -D CGRA_COMPILER -target i386-unknown-linux-gnu -c -emit-llvm -O2 -fno-tree-vectorize -fno-inline -fno-unroll-loops trmm.c -S -o trmm.ll
+
+opt -gvn -mem2reg -memdep -memcpyopt -lcssa -loop-simplify -licm -loop-deletion -indvars -simplifycfg -mergereturn -indvars trmm.ll -S -o trmm_opt.ll
+
+opt -load ../../build/skeleton/libSkeletonPass.so -fn $1 -skeleton trmm_opt.ll -S -o trmm_opt_instrument.ll
+
+clang -target i386-unknown-linux-gnu -c -emit-llvm -S ../../skeleton/instrumentation/instrumentation.cpp -o instrumentation.ll
+
+llvm-link trmm_opt_instrument.ll instrumentation.ll -o final.ll
+
+llc -filetype=obj final.ll -o final.o
+clang++ -m32 final.o -o final
